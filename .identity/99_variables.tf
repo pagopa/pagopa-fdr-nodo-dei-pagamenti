591d4666cbae69b7fcb0da8dc36ecdc8a68fbe89
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-05-02 14:26:12 +0200

    [NOD-714] feat: add .identity folder for GH managed identity

---
+++ .identity/99_variables.tf
@@ -0,0 +1,66 @@
+locals {
+  github = {
+    org        = "pagopa"
+    repository = "pagopa-fdr-nodo-dei-pagamenti"
+  }
+
+  prefix         = "pagopa"
+  domain         = "fdr"
+  location_short = "weu"
+  product        = "${var.prefix}-${var.env_short}"
+
+  app_name = "github-${local.github.org}-${local.github.repository}-${var.prefix}-${local.domain}-${var.env}-aks"
+
+  aks_cluster = {
+    name                = "${local.product}-${local.location_short}-${var.env}-aks"
+    resource_group_name = "${local.product}-${local.location_short}-${var.env}-aks-rg"
+  }
+
+  container_app_environment = {
+    name           = "${local.prefix}-${var.env_short}-${local.location_short}-github-runner-cae",
+    resource_group = "${local.prefix}-${var.env_short}-${local.location_short}-github-runner-rg",
+  }
+
+  integration_test = {
+    storage_account_name = "${local.prefix}${var.env_short}${local.location_short}sharedtstdtsa"
+    storage_account_rg = "${local.prefix}-${var.env_short}-${local.location_short}-shared-tst-dt-rg"
+    reports_folder = local.github.repository
+  }
+}
+
+variable "env" {
+  type = string
+}
+
+variable "env_short" {
+  type = string
+}
+
+variable "prefix" {
+  type    = string
+  default = "pagopa"
+  validation {
+    condition = (
+      length(var.prefix) <= 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
+
+variable "github_repository_environment" {
+  type = object({
+    protected_branches     = bool
+    custom_branch_policies = bool
+    reviewers_teams        = list(string)
+  })
+  description = "GitHub Continuous Integration roles"
+  default = {
+    protected_branches     = false
+    custom_branch_policies = true
+    reviewers_teams        = ["pagopa-team-core"]
+  }
+}
+
+variable "tags" {
+  type = map(any)
+}
---
+++ .identity/env/uat/terraform.tfvars
@@ -0,0 +1,12 @@
+prefix    = "pagopa"
+env       = "uat"
+env_short = "u"
+location  = "westeurope"
+
+tags = {
+  CreatedBy   = "Terraform"
+  Environment = "Uat"
+  Owner       = "pagoPA"
+  Source      = "https://github.com/pagopa/pagopa-fdr-nodo-dei-pagamenti"
+  CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
+}
