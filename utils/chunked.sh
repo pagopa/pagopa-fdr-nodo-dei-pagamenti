0053208a2b04ab7c737f22f847b54f9d2381b182
Francesco <cesareo.francesco@gmail.com>
2025-01-29 13:23:43 +0100

    added azure-mongo to prf config-app

--- utils/chunked.sh
+++ utils/chunked.sh
@@ -1,9 +1,12 @@
 # https://everything.curl.dev/http/post/chunked
 
 FILENAME=$1
-#curl  -H "Transfer-Encoding: chunked" --location --request POST -d @${FILENAME} 'https://api.uat.platform.pagopa.it/nodo/nodo-per-psp/v1' \
-curl  -H "Transfer-Encoding: chunked" --location --request POST -d @${FILENAME} 'localhost:8088/webservices/input' \
+#curl  -H "Transfer-Encoding: chunked" --location --request POST -d @${FILENAME} 'localhost:8088/webservices/input' \
+#curl  -H "Transfer-Encoding: chunked" --location --request POST -d @${FILENAME} 'https://api.dev.platform.pagopa.it/nodo/nodo-per-psp/v1' \
+curl  -H "Transfer-Encoding: chunked" --location --request POST -d @${FILENAME} 'https://api.uat.platform.pagopa.it/nodo-ndp/nodo-per-psp/v1' \
 --header 'SOAPAction: nodoInviaFlussoRendicontazione' \
---header 'Content-Type: application/xml'
+--header 'Content-Type: application/xml' \
+--header 'Ocp-Apim-Subscription-Key: f524b800435c4dc290bdbd1778f15b21'
 
-rm ${FILENAME}
+
+#rm ${FILENAME}
