4445b22ae35a3eb395b5c8b599b9b4acd7606d35
Francesco <cesareo.francesco@gmail.com>
2025-02-17 19:17:26 +0100

    script to insert data in rendicontazione table

---
+++ utils/rendicontazione_insert.py
@@ -0,0 +1,29 @@
+import psycopg2
+import csv
+import os
+
+conn = psycopg2.connect(
+    dbname="fdr",
+    user="fdr",
+    password=os.getenv("password"),
+    host=os.getenv("host"),
+    port=os.getenv("port")
+)
+cursor = conn.cursor()
+
+query = ("INSERT INTO fdr.rendicontazione (optlock, psp, dominio, id_flusso, data_ora_flusso, stato, inserted_timestamp) "
+         "VALUES(0, %s, %s, %s, %s, %s, %s)")
+
+with open('rendicontazione.csv', mode='r', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader) # skip header
+    for row in csv_reader:
+
+        cursor.execute(query, row)
+        conn.commit()
+        break
+
+
+conn.close()
+
+print("Data saved!")
