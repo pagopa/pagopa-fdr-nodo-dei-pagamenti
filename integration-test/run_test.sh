588d445b351dee035c4cd0382f6314edf900298b
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-02-20 18:17:12 +0100

    fix

--- integration-test/run_test.sh
+++ integration-test/run_test.sh
@@ -9,32 +9,32 @@ else
     dest="api.${ENV}."
     sed "s/api.dev./$dest/g" config/${ENV}.json > config/${ENV}.json.bkp
     sleep 1
-    mv config/${ENV}.json.bkp config/${ENV}.json
+    mv config/${ENV}.json.bkp config/dev.json
     sleep 1
 fi
 
-if [ -z $TAGS ]
-then
-  TAGS="runnable"
-fi
-
-if [ -z $JUNIT ]
-then
-  junit=""
-else
-  junit="--junit-directory=junit --junit"
-fi
-
-echo "Run test ..."
-rm -rf results junit
-
-behave --format allure_behave.formatter:AllureFormatter -o results $junit --tags=$TAGS --summary --show-timings -v
-
-rm -rf results/history && cp -R reports/history results/history 2>/dev/null
-
-allure generate results -o reports --clean
-
-if ! [ -z $ENV ]
-then
-  mv config/${ENV}.json.orig config/${ENV}.json
-fi
+#if [ -z $TAGS ]
+#then
+#  TAGS="runnable"
+#fi
+#
+#if [ -z $JUNIT ]
+#then
+#  junit=""
+#else
+#  junit="--junit-directory=junit --junit"
+#fi
+#
+#echo "Run test ..."
+#rm -rf results junit
+#
+#behave --format allure_behave.formatter:AllureFormatter -o results $junit --tags=$TAGS --summary --show-timings -v
+#
+#rm -rf results/history && cp -R reports/history results/history 2>/dev/null
+#
+#allure generate results -o reports --clean
+#
+#if ! [ -z $ENV ]
+#then
+#  mv config/${ENV}.json.orig config/${ENV}.json
+#fi
