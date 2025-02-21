6ede8a8225d08991ddfd9b680bffe4e0e9f32697
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-02-21 11:45:08 +0100

    fix

--- integration-test/run_test.sh
+++ integration-test/run_test.sh
@@ -13,28 +13,28 @@ else
     sleep 1
 fi
 
-#if [ -z $TAGS ]
-#then
-#  TAGS="runnable"
-#fi
-#
-#if [ -z $JUNIT ]
-#then
-#  junit=""
-#else
-#  junit="--junit-directory=junit --junit"
-#fi
-#
-#echo "Run test ..."
-#rm -rf results junit
-#
-#behave --format allure_behave.formatter:AllureFormatter -o results $junit --tags=$TAGS --summary --show-timings -v
-#
-#rm -rf results/history && cp -R reports/history results/history 2>/dev/null
-#
-#allure generate results -o reports --clean
-#
-#if ! [ -z $ENV ]
-#then
-#  mv config/${ENV}.json.orig config/${ENV}.json
-#fi
+if [ -z $TAGS ]
+then
+  TAGS="runnable"
+fi
+
+if [ -z $JUNIT ]
+then
+  junit=""
+else
+  junit="--junit-directory=junit --junit"
+fi
+
+echo "Run test ..."
+rm -rf results junit
+
+behave --format allure_behave.formatter:AllureFormatter -o results $junit --tags=$TAGS --summary --show-timings -v
+
+rm -rf results/history && cp -R reports/history results/history 2>/dev/null
+
+allure generate results -o reports --clean
+
+if ! [ -z $ENV ]
+then
+  mv config/${ENV}.json.orig config/${ENV}.json
+fi
