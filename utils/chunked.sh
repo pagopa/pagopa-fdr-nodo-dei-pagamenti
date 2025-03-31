e00af7540b2bc1cafff9c2d9553843786a473071
Matteo Raiola <matteo.raiola@nttdata.com>
2025-03-31 20:58:37 +0200

    fix: [PAGOPA-2666] manual integration test refactor

--- utils/chunked.sh
+++ utils/chunked.sh
@@ -7,10 +7,10 @@ FILENAME=$1 POST_URL=$2 SUBKEY=$3 CI=$4 FLOW_ID=$5 PSP=$6 FD3_GET_URL=$7
 curl  -H "Transfer-Encoding: chunked" --location --request POST -d @"${FILENAME}" "${POST_URL}" \
 --header 'SOAPAction: nodoInviaFlussoRendicontazione' \
 --header 'Content-Type: application/xml' \
---header `Ocp-Apim-Subscription-Key: ${SUBKEY}`
+--header "Ocp-Apim-Subscription-Key: ${SUBKEY}"
 
 echo "Flow correctly sent by PSP ${PSP} with ID ${FLOW_ID} to ${CI} via ${POST_URL}";
 echo "After a while you can use the below curl to verify the flow status";
-echo "curl --location --request GET ${FD3_GET_URL}/organizations/{CI}/fdrs/{FLOW_ID}/revisions/1/psps/{PSP} --header 'Accept: application/json' --header 'Ocp-Apim-Subscription-Key: <Insert Correct SUBKEY>' ";
+echo "curl --location --request GET ${FD3_GET_URL}/organizations/${CI}/fdrs/${FLOW_ID}/revisions/1/psps/${PSP} --header 'Accept: application/json' --header 'Ocp-Apim-Subscription-Key: <Insert Correct SUBKEY>' ";
 
 #rm ${FILENAME}
--- utils/nodoInviaFlussoRendicontazione.sh
+++ utils/nodoInviaFlussoRendicontazione.sh
@@ -1,3 +1,3 @@
 
-#NUM_PAYMENTS=$1 ENVIRONMENT=$2 SUBKEY=$3 node base64.js | xargs bash chunked.sh
-NUM_PAYMENTS=$1 ENVIRONMENT=$2 SUBKEY=$3 node base64.js
+NUM_PAYMENTS=$1 ENVIRONMENT=$2 SUBKEY=$3 node base64.js | xargs bash chunked.sh
+#NUM_PAYMENTS=$1 ENVIRONMENT=$2 SUBKEY=$3 node base64.js
