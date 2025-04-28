a31958ea4ca049196ccdf46deefd6fae7af901d4
giomella <giomella@emeal.nttdata.com>
2025-04-28 12:22:08 +0200

    [PAGOPA-3000] fix unit tests

--- fdr/src/test/scala/eu/sia/pagopa/RestRendicontazioniTests.scala
+++ fdr/src/test/scala/eu/sia/pagopa/RestRendicontazioniTests.scala
@@ -4,6 +4,7 @@ import akka.http.javadsl.model.StatusCodes
 import eu.sia.pagopa.common.util.Util.gzipContent
 import eu.sia.pagopa.common.util.{RandomStringUtils, Util}
 import eu.sia.pagopa.testutil.TestItems
+import it.pagopa.config.{ConfigDataV1, ConfigurationKey, CreditorInstitution}
 
 import java.time.Instant
 import java.time.format.DateTimeFormatter
@@ -11,6 +12,8 @@ import java.util.Base64
 
 //@org.scalatest.Ignore
 class RestRendicontazioniTests() extends BaseUnitTest {
+
+  type ConfigData = ConfigDataV1
 
   "convertFlussoRendicontazione" must {
     "ok" in {
@@ -21,9 +24,10 @@ class RestRendicontazioniTests() extends BaseUnitTest {
 
       val jsonContent = convertFlussoRendicontazionePayload(idFlusso, String.valueOf(date.getEpochSecond), date.toString, regulation)
       val encodedCompressedFlow = new String(Base64.getEncoder.encode(gzipContent(jsonContent.getBytes)))
-      val payload = s"""{
-         |  "payload": "$encodedCompressedFlow",
-         |  "encoding": "base64"
+      val payload =
+        s"""{
+           |  "payload": "$encodedCompressedFlow",
+           |  "encoding": "base64"
       }""".stripMargin
 
       await(
@@ -37,6 +41,71 @@ class RestRendicontazioniTests() extends BaseUnitTest {
         )
       )
     }
+    "not forward to Nexi when reportingFtpEnabled is false" in {
+      val date = Instant.now()
+      val random = RandomStringUtils.randomNumeric(9)
+      val idFlusso = s"${date}${TestItems.PSP}-$random"
+      val regulation = "1234567890"
+
+      val jsonContent = convertFlussoRendicontazionePayload(idFlusso, String.valueOf(date.getEpochSecond), date.toString, regulation)
+      val encodedCompressedFlow = new String(Base64.getEncoder.encode(gzipContent(jsonContent.getBytes)))
+      val payload = s"""{
+         |  "payload": "$encodedCompressedFlow",
+         |  "encoding": "base64"
+      }""".stripMargin
+
+      val ddataMap: ConfigData = ConfigDataV1(
+        version = "1.0",
+        creditorInstitutions = Map("dominioTest" -> CreditorInstitution(
+          creditorInstitutionCode = "dominioTest",
+          enabled = true,
+          businessName = Some("Test Business"),
+          description = Some("Test Description"),
+          address = None, // o Some(CreditorInstitutionAddress(...)) se hai questa case class
+          pspPayment = false,
+          reportingFtp = false,
+          reportingZip = false
+        )),
+        creditorInstitutionBrokers = Map.empty,
+        stations = Map.empty,
+        creditorInstitutionStations = Map.empty,
+        encodings = Map.empty,
+        creditorInstitutionEncodings = Map.empty,
+        ibans = Map.empty,
+        creditorInstitutionInformations = Map.empty,
+        psps = Map.empty,
+        pspBrokers = Map.empty,
+        paymentTypes = Map.empty,
+        pspChannelPaymentTypes = Map.empty,
+        plugins = Map.empty,
+        pspInformationTemplates = Map.empty,
+        pspInformations = Map.empty,
+        channels = Map.empty,
+        cdsServices = Map.empty,
+        cdsSubjects = Map.empty,
+        cdsSubjectServices = Map.empty,
+        cdsCategories = Map.empty,
+        configurations = Map(
+          "GLOBAL-validate_input" -> ConfigurationKey("", "", "false", None)
+        ),
+        ftpServers = Map.empty,
+        languages = Map.empty,
+        gdeConfigurations = Map.empty,
+        metadataDict = Map.empty
+      )
+
+      await(
+        convertFlussoRendicontazioneActorPerRequest(
+          Some(payload),
+          testCase = Some("OK"),
+          responseAssert = (resp, status) => {
+            assert(status == StatusCodes.OK.intValue)
+            assert(resp.contains("{\"message\":\"OK\"}"))
+          },
+          newdata = Some(ddataMap)
+        )
+      )
+    }
     "ko fdr fase3 error in date format" in {
       val date = Instant.now()
       val random = RandomStringUtils.randomNumeric(9)
@@ -45,9 +114,10 @@ class RestRendicontazioniTests() extends BaseUnitTest {
 
       val jsonContent = convertFlussoRendicontazionePayload(idFlusso, String.valueOf(date.toEpochMilli), date.toString, regulation)
       val encodedCompressedFlow = new String(Base64.getEncoder.encode(gzipContent(jsonContent.getBytes)))
-      val payload = s"""{
-                       |  "payload": "$encodedCompressedFlow",
-                       |  "encoding": "base64"
+      val payload =
+        s"""{
+           |  "payload": "$encodedCompressedFlow",
+           |  "encoding": "base64"
       }""".stripMargin
 
       await(
@@ -69,8 +139,9 @@ class RestRendicontazioniTests() extends BaseUnitTest {
 
       val jsonContent = convertFlussoRendicontazionePayload(idFlusso, String.valueOf(date.getEpochSecond), date.toString, regulation)
       val encodedCompressedFlow = new String(Base64.getEncoder.encode(gzipContent(jsonContent.getBytes)))
-      val payload = s"""{
-                       |  "payload": "$encodedCompressedFlow"
+      val payload =
+        s"""{
+           |  "payload": "$encodedCompressedFlow"
       }""".stripMargin
 
       await(
@@ -84,31 +155,32 @@ class RestRendicontazioniTests() extends BaseUnitTest {
         )
       )
     }
-  }
 
-  "ko fdr fase3 bad request" in {
-    val date = Instant.now()
-    val random = RandomStringUtils.randomNumeric(9)
-    val idFlusso = s"${date}${TestItems.PSP}-$random"
-    val jsonContent =
-      s"""{
-         |  "fdr": "$idFlusso",
+    "ko fdr fase3 bad request" in {
+      val date = Instant.now()
+      val random = RandomStringUtils.randomNumeric(9)
+      val idFlusso = s"${date}${TestItems.PSP}-$random"
+      val jsonContent =
+        s"""{
+           |  "fdr": "$idFlusso",
       }""".stripMargin
-    val encodedCompressedFlow = new String(Base64.getEncoder.encode(gzipContent(jsonContent.getBytes)))
-    val payload = s"""{
-                     |  "payload": "$encodedCompressedFlow"
+      val encodedCompressedFlow = new String(Base64.getEncoder.encode(gzipContent(jsonContent.getBytes)))
+      val payload =
+        s"""{
+           |  "payload": "$encodedCompressedFlow"
       }""".stripMargin
 
-    await(
-      convertFlussoRendicontazioneActorPerRequest(
-        Some(payload),
-        testCase = Some("KO"),
-        responseAssert = (resp, status) => {
-          assert(status == StatusCodes.BAD_REQUEST.intValue())
-          assert(resp.contains("{\"error\":\"The provided FdR 3 flow JSON is invalid:"))
-        }
+      await(
+        convertFlussoRendicontazioneActorPerRequest(
+          Some(payload),
+          testCase = Some("KO"),
+          responseAssert = (resp, status) => {
+            assert(status == StatusCodes.BAD_REQUEST.intValue())
+            assert(resp.contains("{\"error\":\"The provided FdR 3 flow JSON is invalid:"))
+          }
+        )
       )
-    )
+    }
   }
 
   "registerFdrForValidation" must {
@@ -212,6 +284,7 @@ class RestRendicontazioniTests() extends BaseUnitTest {
       val nodoInviaFlussoRendicontazioneReplaced = nodoInviaFlussoRendicontazione
         .replaceAll("\"", "\\\\\"")
         .replaceAll("\n", "")
+        .replaceAll("\r", "")
 
       val payload =
         s"""
@@ -254,6 +327,7 @@ class RestRendicontazioniTests() extends BaseUnitTest {
       val nodoInviaFlussoRendicontazioneReplaced = nodoInviaFlussoRendicontazione
         .replaceAll("\"", "\\\\\"")
         .replaceAll("\n", "")
+        .replaceAll("\r", "")
 
       val payload =
         s"""
