c17acfd271fb39c72a3706ff764a6ec9c973c1dd
svariant <samuele.varianti@nttdata.com>
2026-03-17 17:11:12 +0100

    fix: [PIDM-1482] Fix error message condition

--- fdr/src/main/scala/eu/sia/pagopa/rendicontazioni/actor/rest/ConvertFlussoRendicontazioneActorPerRequest.scala
+++ fdr/src/main/scala/eu/sia/pagopa/rendicontazioni/actor/rest/ConvertFlussoRendicontazioneActorPerRequest.scala
@@ -316,7 +316,7 @@ case class ConvertFlussoRendicontazioneActorPerRequest(repositories: Repositorie
               responseBody.esito.equals("OK") ||
                 responseBody.fault.exists(f =>
                   f.faultCode == "PPT_SEMANTICA" &&
-                    f.description.exists(desc => desc.contains("gia' presente") || desc.contains("già aggiornato con dataOraFlusso"))
+                    f.description.exists(desc => desc.contains("gia' presente") || desc.contains("più aggiornato con dataOraFlusso"))
                 )
             ) {
               Future.successful()
